ce5abee112ac5a4d2f66b67abc1f223d991a3347
NopyJF <Nithichai@users.noreply.github.com>
2018-01-28 23:06:17 +0700

    Add csv file & save func from friends branches

--- csv_to_db.py
+++ csv_to_db.py
@@ -1,31 +1,56 @@
 import sqlite3 as db
+import os
+import csv
 
 class MainTable:
-	def __init__(self):
-		self.conn = db.connect("gpa.db")
+	def __init__(self, database):
+		self.conn = db.connect(database)
+		self.table = "Main"
 
 	def select_all(self):
-		# return list of all elements
-		pass
+		con = self.conn
+		cur = con.cursor()
+		cur.execute("SELECT * FROM main_table")
+		return cur.fetchall()
 
 	def insert(self, data):
-		# input : row of main table
-		# return status
-		pass
+		con = self.conn
+		with con:
+			cur = con.cursor()
+			try:
+				cur.execute("PRAGMA foreign_keys = ON")
+				cur.execute("INSERT INTO " + self.table + " VALUES (?,?,?,?,?)",data)
+			except Exception as e:
+				print (data)
+				print (e)
 
-	def update(self, headers, data):
+	def update(self, data):
+		con = self.conn
+		with con:
+			cur = con.cursor()
+			try:
+				cur.execute("UPDATE main_table SET grade = ? where student_id = ? AND course_code = ?", (data, stu_id, cour_code))
+			except Exception as e:
+				print (data)
+				print (e)
+
+	def delete(self, data):
 		# input : list of header and data
 		# return status
 		pass
-
-	def delete(self, headers, data):
-		# input : list of header and data
-		# return status
-		pass
-
-	def close():
-		conn.close()
 
 ################
 # read csv and insert to db here
 ################
+main_TB = MainTable("KMUTNBdb.db")
+file_list = []
+for (dirpath, dirnames, filenames) in os.walk('.'):
+        file_list.extend(filenames)
+        break
+csv_file_list = [file for file in file_list if file.endswith('.csv')]
+for file in csv_file_list:
+        print('reading file:', file)
+        with open(file, 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for row in csv_reader:
+                        main_TB.insert(row)
