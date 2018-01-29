ad356503b1a4e2ab7d846efe5d8878ba250274fe
NopyJF <Nithichai@users.noreply.github.com>
2018-01-29 22:55:51 +0700

    Fixable table name

--- csv_to_db.py
+++ csv_to_db.py
@@ -10,8 +10,19 @@ class MainTable:
 	def select_all(self):
 		con = self.conn
 		cur = con.cursor()
-		cur.execute("SELECT * FROM main_table")
+		cur.execute("SELECT * FROM " + self.table)
 		return cur.fetchall()
+
+	def show_all(self):
+		list_main = self.select_all()
+		print("==========================")
+		for data in list_main:
+			print("student_id : %s" % data[0])
+			print("year : %s" % data[1])
+			print("semester : %s" % data[2])
+			print("couser_code : %s" % data[3])
+			print("grade_char : %s" % data[4])
+			print("==========================")
 
 	def insert(self, data):
 		con = self.conn
@@ -29,7 +40,7 @@ class MainTable:
 		with con:
 			cur = con.cursor()
 			try:
-				cur.execute("UPDATE main_table SET grade = ? where student_id = ? AND course_code = ?", (data, stu_id, cour_code))
+				cur.execute("UPDATE "+ self.table + " SET grade_char = ? WHERE student_id = ? AND course_code = ?", data)
 			except Exception as e:
 				print (data)
 				print (e)
@@ -43,14 +54,15 @@ class MainTable:
 # read csv and insert to db here
 ################
 main_TB = MainTable("KMUTNBdb.db")
-file_list = []
-for (dirpath, dirnames, filenames) in os.walk('.'):
-        file_list.extend(filenames)
-        break
-csv_file_list = [file for file in file_list if file.endswith('.csv')]
-for file in csv_file_list:
-        print('reading file:', file)
-        with open(file, 'r') as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=',')
-                for row in csv_reader:
-                        main_TB.insert(row)
+print main_TB.update(["B+", "5801012620046", "010123101"])
+# file_list = []
+# for (dirpath, dirnames, filenames) in os.walk('.'):
+#         file_list.extend(filenames)
+#         break
+# csv_file_list = [file for file in file_list if file.endswith('.csv')]
+# for file in csv_file_list:
+#         print('reading file:', file)
+#         with open(file, 'r') as csv_file:
+#                 csv_reader = csv.reader(csv_file, delimiter=',')
+#                 for row in csv_reader:
+#                         main_TB.insert(row)
